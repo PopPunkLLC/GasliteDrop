c7c2ee6650424eef4a556fc7b00fb7663a450283
Scott Mitchell <scottymitch88@gmail.com>
2025-03-04 16:05:03 -0500

    Add AGW support

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -13,7 +13,14 @@ import {
   abstract,
   sepolia,
 } from "wagmi/chains";
-import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+import { abstractWallet } from "@abstract-foundation/agw-react/connectors";
+import {
+  connectorsForWallets,
+  getDefaultWallets,
+} from "@rainbow-me/rainbowkit";
+import { createConfig } from "wagmi";
+import { createClient, http } from "viem";
+import { eip712WalletActions } from "viem/zksync";
 
 export const chains = [
   arbitrum,
@@ -31,9 +38,32 @@ export const chains = [
   ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true" ? [sepolia] : []),
 ];
 
-export const config = getDefaultConfig({
-  appName: "Gaslite Drop",
-  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_API_KEY as string,
+const defaultWallets = getDefaultWallets();
+
+console.log(JSON.stringify(defaultWallets, null, 2));
+
+export const config = createConfig({
   chains,
+  connectors: connectorsForWallets(
+    [
+      ...defaultWallets.wallets,
+      {
+        groupName: "Abstract",
+        wallets: [abstractWallet],
+      },
+    ],
+    {
+      appName: "Gaslite Drop",
+      projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_API_KEY as string,
+      appDescription:
+        "The most efficient airdrop tool. Create and Deploy ERC20 Bytecode. Bulk transfer ERC20s, ERC721s, ERC1155s across many chains.",
+    },
+  ),
   ssr: true,
+  client({ chain }) {
+    return createClient({
+      chain,
+      transport: http(),
+    }).extend(eip712WalletActions());
+  },
 });
